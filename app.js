73659adb0742854f4ba457de127c72442238e945
Patrick <patrick.henn@nx-technologies.com>
2020-10-14 16:41:46 +0200

    bodyParser

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 let express = require('express');
+let bodyParser = require('body-parser');
 let app = express();
+
 const { countAmountOfEachCharacter } = require('./countAmountOfEachCharacter/script.js');
 //const splitScrip = require('./splitOddAndEven/script.js');
 const { splitOddAndEven } = require('./splitOddAndEven/script.js');
 const { calculator } = require('./public/script.js');
 
-let bodyParser = require('body-parser');
 
 app.use(express.static(__dirname +'/public'));
 
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(bodyParser());
 
-app.post('/', function(req, res) {
-	resp.end(JSON.stringify(req.body));
+app.post('/countAmountOfEachCharacter/test', function(req, res) {
+	console.log('es geht');
+	res.end(JSON.stringify(req.body));
 });
 
 
@@ -23,26 +25,26 @@ console.log(req.params, req.query)
 });
 
 app.get('/countAmountOfEachCharacter', function(req, res) {
-console.log(req.params, req.query)
-const result = countAmountOfEachCharacter(req.query.input ? req.query.input : '');
+	console.log(req.params, req.query)
+	const result = countAmountOfEachCharacter(req.query.input ? req.query.input : '');
   res.sendFile(__dirname + "/countAmountOfEachCharacter/index.html");
 });
-
-/*app.get('/splitOddAndEven', function(req, res) {
-console.log(req.params, req.query)
-const result = splitScrip(req.query.input ? req.query.input : '');
+/*
+app.get('/splitOddAndEven', function(req, res) {
+	console.log(req.params, req.query)
+	const result = splitScrip(req.query.input ? req.query.input : '');
   res.sendFile(__dirname + "/splitOddAndEven/index.html");
 });*/
 
 app.get('/splitOddAndEven', function(req, res) {
-console.log(req.params, req.query)
-const result = splitOddAndEven(req.query.input ? req.query.input : '');
+	console.log(req.params, req.query)
+	const result = splitOddAndEven(req.query.input ? req.query.input : '');
   res.sendFile(__dirname + "/splitOddAndEven/index.html");
 });
 
 app.get('/Taschenrechner', function(req, res) {
-console.log(req.params, req.query)
-const result = calculator(req.query.input ? req.query.input : '');
+	console.log(req.params, req.query)
+	const result = calculator(req.query.input ? req.query.input : '');
   res.sendFile(__dirname + "/Taschenrechner/index.html");
 });
 
