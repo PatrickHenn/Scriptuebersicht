31110df98ad8078989edf67395267aa7777f9cef
Patrick <patrick.henn@nx-technologies.com>
2020-10-22 17:34:20 +0200

    umstellung auf pug test

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
-const pug = require('pug');
 
 
-const compiledFunction = pug.compileFile('template.pug');
+
+
 const { countAmountOfEachCharacter } = require('./countAmountOfEachCharacter/script.js');
 const { splitOddAndEven } = require('./splitOddAndEven/script.js');
 //const splitScrip = require('./splitOddAndEven/script.js');
@@ -14,11 +14,12 @@ const { splitOddAndEven } = require('./splitOddAndEven/script.js');
 
 app.use(express.static(__dirname +'/public'));
 app.use(bodyParser.urlencoded({extended: true}));
-
+app.set('view engine', 'pug');
+/*
 
 app.post('/countAmountOfEachCharacter/ergebnis', function(req, res) {
 	console.log('es spricht buchstaben');
-	pug.render('p Hello world!');
+//	pug.render('p Hello world!');
 	res.end(JSON.stringify(req.body));
 });
 
@@ -27,9 +28,11 @@ app.post('/splitOddAndEven/ergebnis', function(req, res) {
 	res.end(JSON.stringify(req.body));
 });
 
+*/
+app.get('/test', function (req, res) {
+  res.render('index', { test: 'Hey', message: 'Hello there!'});
+});
 
-
-//GET aufrufe -> funftionen + index
 app.get('/', function(req, res) {
 console.log(req.params, req.query)
   res.sendFile(__dirname + "/index.html");
@@ -51,13 +54,6 @@ app.get('/Taschenrechner', function(req, res) {
 	console.log(req.params, req.query)
   res.sendFile(__dirname + "/Taschenrechner/index.html");
 });
-
-/*
-app.get('/splitOddAndEven', function(req, res) {
-	console.log(req.params, req.query)
-	const result = splitScrip(req.query.input ? req.query.input : '');
-  res.sendFile(__dirname + "/splitOddAndEven/index.html");
-});*/
 
 //start server
 app.listen(3000, function() {
