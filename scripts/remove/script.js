0a2079e2b70d060cc71e0d90ee5e4e94a08e84bf
Patrick <patrick.henn@nx-technologies.com>
2020-11-17 13:10:48 +0100

    einrcücken

--- scripts/remove/script.js
+++ scripts/remove/script.js
@@ -5,22 +5,21 @@ function remove(_string){
   let newString = "";
   for (x=0;x<_string.length;x++){
     console.log(_string[x],x)
-    if ( _string.length == x){}
-    else{
+    if (_string[x] == '!'){
+      _string[x]=''
+    } else{
     newString += _string[x]
    console.log('hooo',newString); 
     }
-    console.log('hooo',newString);
   }
  return newString
 }
-/*let _string ="dasdsa!"
+let _string ="dasdsa!"
 const log = remove(_string);
-
     console.log('log',log);
 
 module.exports ={ remove };
-*/
+
 /*
 ! am ende entfernen dafür muss man das ende herausfinden durch length -1 und dann if ==! doppelt loopen und entfernen
 
