19600a1b4a3ed72f3e4970742fc714fa35cc1b18
Patrick Henn <patrickhenn@MacBook-Air-von-Patrick.fritz.box>
2021-03-04 11:56:00 +0100

    tabelle

--- scripts/createOrder.js
+++ scripts/createOrder.js
@@ -1,9 +1,6 @@
 require('dotenv').config();
-const { response } = require('express')
 const request = require('request-promise-native');
 const {post,get,orgaId} = require("./lib/request.js");
-let orderId = 'doffmPY05';
-// let orgaId = '';
 
 async function createOrder(test) {
   let _createOrder = await post('order/',
@@ -12,26 +9,24 @@ async function createOrder(test) {
       "name": test.name,
       "price": test.price,
       "recipient": 'patrick.henn+res@nx-technologies.com'
-    })
+    });
   console.log('createOrder=',_createOrder);
   return createOrder, test.name
 };
 
-let lastId = ''
-let obj = []
-
 async function getOrder(){
-let _get = await get('order/?orgId='+`${orgaId}`); 
+let _get = await get('order/?orgId='+`${orgaId}`);
+    let obj = [];
     obj = _get.list.map((X) => {
     const data = {
     lastId :X.id,
     lastName :X.name,
     lastPrice :X.total,
-    lastRecipient : X.recipient.length?X.recipient[0].email:"" // 'patrick.henn+code@nx-technologies.com'//_get.list[x].recipient[0].email;
-    };
+    lastRecipient : X.recipient.length?X.recipient[0].email:"" 
+    };  
     return data
   });
-  console.log('OOOOO',obj)
+  return obj
 };
 
 module.exports ={createOrder, getOrder}; 
