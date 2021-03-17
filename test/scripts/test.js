29a65c4a32553b1bb5ba02a8b103c19ddf7162af
Patrick Henn <patrickhenn@Air-von-Patrick.fritz.box>
2021-03-17 14:06:25 +0100

    mocha json test

--- test/scripts/test.js
+++ test/scripts/test.js
@@ -5,21 +5,25 @@ const assert = require('assert');
 const { createOrder } = require('../../scripts/createOrder');
 const { changeOrder } = require('../../scripts/lib/order');
 
-describe('createOrder', () => {
+describe('Test 1', () => {
   it('createOrder should create a order', async () => {
     const test1 = await createOrder({ name: 'test', price: '2999' });
     assert.equal(test1, '{"orgId":"Hgza28rQ-","name":"test","price":"2999","recipient":"patrick.henn+test@nx-technologies.com"}');
   });
   it('createOrder = string', async () => {
     const test2 = await createOrder({ name: 'test', price: '2999' });
-    assert.equal(typeof test2, 'string');
+    assert.equal(typeof JSON.parse(test2), 'object');
   });
 });
 
-describe('updateContactperson', () => {
+describe('Test 2', () => {
   it('updateContactperson changes the Contactperson', async () => {
     const test3 = await changeOrder({ name: 'TEST' });
-    assert.equal(test3.updateContactperson, '{"contactperson":"patrick.henn+updatecontactperson@nx-technologies.com","orgId":"Hgza28rQ-"}');
+    console.log('erereere', JSON.parse(test3.updateContactperson));
+    assert.equal(JSON.parse(test3.updateContactperson), {
+      contactperson: 'patrick.henn+updatecontactperson@nx-technologies.com',
+      orgId: 'Hgza28rQ-',
+    });
   });
   it('updateName changes the Name', async () => {
     const test4 = await changeOrder({ name: 'TEST' });
